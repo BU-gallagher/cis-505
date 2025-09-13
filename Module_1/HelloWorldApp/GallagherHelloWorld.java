67cbe8b4e15ce7c2c8f65917115de7e568b37c0d
Mark <msgallagher@my365.bellevue.edu>
2025-09-12 20:41:40 -0400

    Added the introductionary setup java script HelloWorldApp.java file.

---
+++ Module_1/HelloWorldApp/GallagherHelloWorld.java
@@ -0,0 +1,28 @@
+
+/*
+    Liang, Y.D. (2020). Introduction to Java Programming and Data Structures:
+    Comprehensive Version (12th ed.). Pearson Education, Inc.
+    Modified by R. Krasso 2021
+    Additional modifications by Mark Gallagher 2025
+*/
+
+
+public class GallagherHelloWorld {
+
+    /*
+     * Main method created with two variables which concludes with the printing the student's first and last name
+     * variable 1 will be first name
+     * variable 2 will be last name
+     * @return prints the results to the console window
+     */
+    public static void main(String[] args) {
+        // Declare string variables for first and last name
+        String firstName = "Mark";
+        String lastName = "Gallagher";
+
+        // Use printf to output the message
+        System.out.printf("Hello World from " + firstName + " " + lastName);
+    }// end main
+}
+
+
